262dbdfea1e6583e8ae0493ced0a2e561fa3e808
jose guilherme <jgnvs6@gmail.com>
2021-03-24 00:43:13 -0300

    add para,eters im unit_control

--- verilog/parts_made/cpu.v
+++ verilog/parts_made/cpu.v
@@ -9,24 +9,58 @@ module cpu(
     wire REG_w;
     wire AB_w;
     wire EPC_w;
+    wire HI_w;
+    wire LO_w;
+    wire MDR_w;
+    wire ALUOut_w;
+    wire [3:0] ALU_op;
+    wire [2:0] Shift_op;
+    wire [1:0] M_SrcA;
+    wire [1:0] M_SrcB;
     wire [1:0] M_EXCEPTION;
     wire [1:0] M_RMEM;
+    wire [1:0] M_WRITE_REG;
+    wire [1:0] M_WRITE_DATA;
+    wire M_Shift_In;
+    wire M_Shift_N;
 
     // Data wires
     wire [31:0] PC_input;
-    wire [31:0] IR_input;
-    wire [31:0] ULA_A_input;
-    wire [31:0] ULA_B_input;
     wire [31:0] PC_out;
-    wire [31:0] MEM_out;
-    wire [31:0] BR_A_out;
-    wire [31:0] BR_B_out;
+    wire [31:0] M_EXCEPTION_out;
+    wire [31:0] IR_input;
+    wire [31:0] MDR_out;
     wire [5:0] OPCODE;
     wire [4:0] RS;
     wire [4:0] RT;
     wire [15:0] OFFSET;
-    wire [31:0] M_EXCEPTION_out;
+    wire [32:00] SE_16_32_out;
+    wire [32:00] SE_1_32_out;
+    wire [32:00] SL_32_out;
     wire [31:0] M_RMEM_out;
+    wire [31:0] MEM_out;
+    wire [31:0] BR_A_out;
+    wire [31:0] BR_B_out;
+    wire [31:0] A_out;
+    wire [31:0] B_out;
+    wire [31:0] ALU_A_input;
+    wire [31:0] ALU_B_input;
+    wire [31:0] HI_LO_input;
+    wire [31:0] ALU_out;
+    wire [31:0] ALUOut_out;
+    wire [31:0] HI_out;
+    wire [31:0] LO_out;
+    wire [31:0] M_ALUA_out;
+    wire [31:0] M_ALUB_out;
+    wire [31:0] M_RMEM_out;
+    wire [31:0] M_Shift_In_out;
+    wire [31:0] Shift_REG_out;
+    wire O;
+    wire N;
+    wire Z;
+    wire ET;
+    wire LT;
+    wire GT;
 
     Registrador PC_(
         clk,
@@ -41,7 +75,7 @@ module cpu(
         reset,
         AB_w,
         BR_B_out,
-        ULA_B_input
+        B_out
     );
 
     Registrador A_(
@@ -49,7 +83,23 @@ module cpu(
         reset,
         AB_w,
         BR_A_out,
-        ULA_A_input
+        A_out
+    );
+
+    Registrador HI_(
+        clk,
+        reset,
+        HI_w,
+        HI_LO_input,
+        HI_out
+    );
+
+    Registrador LO_(
+        clk,
+        reset,
+        LO_w,
+        HI_LO_input,
+        LO_out
     );
 
     Registrador EPC_(
@@ -58,6 +108,22 @@ module cpu(
         PC_w,
         PC_input,
         PC_out
+    );
+
+    Registrador ALUOut_(
+        clk,
+        reset,
+        ALUOut_w,
+        ALU_out,
+        ALUOut_out,
+    );
+
+    Registrador MDR_(
+        clk,
+        reset,
+        MDR_w,
+        MEM_out,
+        MDR_out,
     );
 
     Memoria MEM_(
@@ -79,6 +145,33 @@ module cpu(
         OFFSET
     );
 
+    sign_extend_16_to_32 SE_16_32_(
+        OFFSET,
+        SE_16_32_out
+    );
+
+    sign_extend_1_to_32 SE_1_32_(
+        LT,
+        SE_1_32_out
+    );
+
+    shift_left2_32 SL_32 (
+        SE_16_32_out,
+        SL_32_out
+    );
+
+    ula32 ALU_(
+        M_ALUA_out,
+        M_ALUB_out,
+        ALU_op,
+        ALU_out, // resultado da operacao
+        O, //Overflow
+        N, //Negativo
+        Z, // quando S for zero 
+        ET, // igual
+        LT, // menor que
+        GT, // maior que 
+    );
 
     mux_exception M_EXCEPTION_(
         M_EXCEPTION,
@@ -92,19 +185,76 @@ module cpu(
         M_EXCEPTION_out,
         M_RMEM_out
     );
+    
+    mux_ulaA M_ALUA_(
+        M_SrcA,
+        PC_out,
+        A_out,
+        B_out,
+        MDR_out,
+        ALU_A_input
+    );
 
+    mux_ulaB M_ALUB_(
+        M_SrcB,
+        B_out,
+        SE_16_32_out,
+        SL_32_out,
+        ALU_B_input
+    );
 
+    mux_ulaB M_ALUB_(
+        M_SrcB,
+        B_out,
+        SE_16_32_out,
+        SL_32_out,
+        ALU_B_input
+    );
 
-    // Banco_reg REG_BASE (
-    //     clk,
-    //     reset,
-    //     REG_w,
-    //     RS, // na verdade vamos usar o mux "mux_readReg1", ainda não sabemos quem são os registradores hi e lo
-    //     RT, 
-    //     WriteReg, // na verdade vamos usar o mux "mux_writeReg", ainda não sabemos quem são os registradores hi,lo e ra
-    //     WriteData, // mux ainda nao completo pq causa da mutiplicacao e divisao
-    //     ReadData1,
-    //     ReadData2
-    // );
+    mux_writeData M_Write_Data_(
+        M_WRITE_REG,
+        RT,
+        OFFSET,
+        RS
+    );
+
+    mux_shiftInput M_shift_In_(
+        M_shift_In,
+        ALU_A_input,
+        ALU_B_input
+    );
+
+    mux_shiftInput M_shift_In_(
+        M_Shift_In,
+        ALU_A_input,
+        ALU_B_input,
+        M_Shift_In_out
+    );
+
+    mux_shiftN M_shift_N_(
+        M_Shift_N,
+        OFFSET,
+        ALU_B_input,
+        M_Shift_N_out
+    );
+
+    RegDesloc Shift_REG_(
+        clk,
+        reset,
+        Shift_op,
+        M_Shift_N_out,
+        M_Shift_In_out,
+        Shift_REG_out
+    );
+
+    mux_writeReg M_Write_Reg_(
+        M_WRITE_DATA,
+        MDR_out,
+        ALUOut_out,
+        HI_out,
+        LO_out,
+        Shift_REG_out,
+        SE_1_32_out
+    );
 
 endmodule
--- verilog/parts_made/ctrl_unit.v
+++ verilog/parts_made/ctrl_unit.v
@@ -48,4 +48,54 @@ module ctrl_unit (
   output reg       Reset//não está no desenho, é intuitivo
 );
 
+  // variaveis
+  reg [1:0] state;
+  reg [2:0] cont;
+
+  // opcodes aliases 
+  parameter NULL  =   6'b000000;
+  parameter BLM   =   6'b000001;
+  parameter J     =   6'b000010;
+  parameter JAL   =   6'b000011;
+  parameter BEQ   =   6'b000100;
+  parameter BNE   =   6'b000101;
+  parameter BLE   =   6'b000110;
+  parameter BGT   =   6'b000111;
+  parameter ADDI  =   6'b001000;
+  parameter ADDIU =   6'b001001;
+  parameter SLTI  =   6'b001010;
+  parameter LUI   =   6'b010000;
+  parameter LB    =   6'b100000;
+  parameter LH    =   6'b100001;
+  parameter LW    =   6'b100011;
+  parameter SB    =   6'b101000;
+  parameter SW    =   6'b101011;
+
+  // functions aliases
+  parameter SLL   =   6'b000000;
+  parameter SRL   =   6'b000010;
+  parameter SRA   =   6'b000011;
+  parameter SLLV  =   6'b000100;
+  parameter XCHG  =   6'b000101;
+  parameter SRAV  =   6'b000111;
+  parameter JR    =   6'b001000;
+  parameter BREAK =   6'b001011;
+  parameter MFLO  =   6'b010010;
+  parameter RTE   =   6'b010011;
+  parameter MULT  =   6'b011000;
+  parameter DIV   =   6'b011010;
+  parameter ADD   =   6'b100000;
+  parameter SUB   =   6'b100010;
+  parameter AND   =   6'b100100;
+  parameter SLT   =   6'b101010;
+
+
+initial begin
+  
+end
+
+always @(posedge clk) begin
+
+end
+
 endmodule
