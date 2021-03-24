9321364c1a3d6c746a6edf8af7bd48c1311ef061
jose guilherme <jgnvs6@gmail.com>
2021-03-23 23:23:38 -0300

    alter length inputs in muxs

--- verilog/parts_made/mux_writeReg.v
+++ verilog/parts_made/mux_writeReg.v
@@ -1,16 +1,16 @@
 module mux_writeReg (
   input wire  [1:0]  selector,
-  input wire  [31:0] input_one,
-  input wire  [31:0] input_two,
-  input wire  [31:0] input_three,
+  input wire  [4:0] input_one,
+  input wire  [15:11] input_two,
+  input wire  [4:0] input_three,
   output wire [31:0] output_final;
 );
 
-  wire [31:0] aux_1;
-  wire [31:0] aux_2;
+  wire [4:0] aux_1;
+  wire [4:0] aux_2;
 
   assign aux_1 = (selector[0]) ? input_two : input_one;
-  assign aux_2 = (selector[0]) ? 32'b00000000000000000000000000011111 : input_three; //ra - input_three
+  assign aux_2 = (selector[0]) ? 5'11111 : input_three; //ra - input_three
   assign output_final = (selector[1]) ? aux_2 : aux_1;
 
   endmodule
